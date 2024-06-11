e3e15d9c25a8a0999f1f51462a50d1f0cb95c947
Andy Cork <andy.cork@gmail.com>
2024-06-11 15:42:10 +0100

    Updates to the capacitor projects

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,4 +1,4 @@
-import { AuthenticationResponseJSON, PublicKeyCredentialCreationOptionsJSON, PublicKeyCredentialRequestOptionsJSON, RegistrationResponseJSON } from '@simplewebauthn/types';
+import type { AuthenticationResponseJSON, PublicKeyCredentialCreationOptionsJSON, PublicKeyCredentialRequestOptionsJSON, RegistrationResponseJSON } from '@simplewebauthn/types';
 
 export interface WebAuthnPlugin {
   isWebAuthnAvailable(): Promise<{ value: boolean }>;
